924187bfd9dedf515b325f832a0c3a132e6f2bed
nishisuke <ryoskns+git@gmail.com>
2024-01-26 17:28:58 +0900

    history

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import {useEffect} from "react";
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
@@ -23,6 +24,7 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <History />
         <Outlet />
         <ScrollRestoration />
         <Scripts />
@@ -30,4 +32,53 @@ export default function App() {
       </body>
     </html>
   );
+}
+
+function History() {
+
+useEffect(() => {
+console.log(window.history.state)
+},[])
+
+const op1 = () => {
+const h = window.history
+h.pushState({aiu: 3}, "what is this 1", "/a?state=1")
+h.pushState({hohoge: 'fjdkas'}, "what is this 2", "/b?state=2")
+h.pushState({hohoge: 'fjdkas'}, "what is this 3", "/c?state=3")
+h.replaceState({kkk: false},  "what is this 3", "/a?state=4")
+}
+
+const state = () => {
+console.log(window.history.state)
+}
+
+const b = () => {
+window.history.back()
+}
+const f = () => {
+window.history.forward()
+
+}
+
+return (
+<div>
+
+<p>
+<a href="/a">a</a>
+</p>
+<p>
+<a href="/b">b</a>
+</p>
+<p>
+<a href="/c">c</a>
+</p>
+
+<button onClick={op1}>aiue</button>
+<button onClick={state}>getstate</button>
+
+<button onClick={b}>back</button>
+<button onClick={f}>forward</button>
+</div>
+
+)
 }
